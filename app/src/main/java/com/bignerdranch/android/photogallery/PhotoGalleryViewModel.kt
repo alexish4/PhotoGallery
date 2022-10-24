3bdcb79d237e8f15c95380cb59d071a99e5deb77
alexish4 <alexish4@cpp.edu>
2022-10-23 23:35:08 -0700

    Seems to still be working

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -1,26 +1,30 @@
 package com.bignerdranch.android.photogallery
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
 import retrofit2.http.Query
 
-class PhotoGalleryViewModel : ViewModel() {
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
 
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
 
     init {
-        mutableSearchTerm.value = "planets"
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
 
         galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm ->
-            flickrFetchr.searchPhotos(searchTerm)
+            if (searchTerm.isBlank()) {
+                flickrFetchr.fetchPhotos()
+            }
+            else {
+                flickrFetchr.searchPhotos(searchTerm)
+            }
         }
     }
 
     fun fetchPhotos(query: String = "") {
+        QueryPreferences.setStoredQuery(app, query)
         mutableSearchTerm.value = query
     }
 }
